cba10d806d3b133c9a7444ee0bb989bde51c78e0
uday Ranjan <ranjanuday3@gmail.com>
2026-04-29 15:13:20 +0530

    yes fix

--- db.py
+++ db.py
@@ -1,9 +1,10 @@
 import mysql.connector
+import os
 
 def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Uday@123",
+        password=os.getenv("DB_PASSWORD", "Uday@123"),
         database="student_db"
     )
